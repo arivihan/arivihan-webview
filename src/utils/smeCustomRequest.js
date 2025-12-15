126416b51f9bc0420fe3c0bc064e3315394df3ab
Sonu <skprashant.iitr@gmail.com>
2025-12-15 17:13:01 +0530

    Refactor request utility functions for consistency
    Refactored smeCustomRequest and analyticsCustomRequest to use consistent formatting and modern JavaScript syntax. Removed duplicate header keys and improved code readability.

--- src/utils/smeCustomRequest.js
+++ src/utils/smeCustomRequest.js
@@ -1,74 +1,62 @@
-
 function smeCustomRequest(path, method = "POST", body = {}) {
+  let headers = {
+    userId: localStorage.getItem("id") ?? "",
+    "content-Type": "application/json",
+    Authorization: "Bearer " + localStorage.getItem("token") ?? "",
+    token: localStorage.getItem("token") ?? "",
+  };
 
-    let headers = {
-        "token": localStorage.getItem("token") ?? "",
-        "userId": localStorage.getItem('id') ?? "",
-        "content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("token") ?? "",
-        "token": localStorage.getItem("token") ?? ""
-    }
+  let options = {
+    method: method,
+    headers: headers,
+  };
 
-    let options = {
-        method: method,
-        headers: headers,
-    }
+  if (method === "POST") {
+    options["body"] = JSON.stringify(body);
+  }
 
-    if (method === "POST") {
-        options['body'] = JSON.stringify(body)
-    }
-
-    try{
- return fetch(`https://prod.arivihan.com/internal-metrics${path}`, options)
-        .then(res => {
-            if (res.status === 200) {
-                return res.json()
-            } else if (res.status === 401) {
-                console.log(res);
-                return res.json();
-            }
-        })
-        .then(json => {
-            return json;
-        })
-    }catch{
-
-    }
-   
+  try {
+    return fetch(`https://prod.arivihan.com/internal-metrics${path}`, options)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else if (res.status === 401) {
+          console.log(res);
+          return res.json();
+        }
+      })
+      .then((json) => {
+        return json;
+      });
+  } catch {}
 }
-
 
 function analyticsCustomRequest(path, method = "GET", body = {}) {
+  let headers = {
+    Accept: "application/json",
+  };
 
-    let headers = {
-        'Accept': 'application/json'
-    }
+  let options = {
+    method: method,
+    headers: headers,
+  };
 
-    let options = {
-        method: method,
-        headers: headers,
-    }
+  if (method === "POST") {
+    options["body"] = JSON.stringify(body);
+  }
 
-    if (method === "POST") {
-        options['body'] = JSON.stringify(body)
-    }
-
-    return fetch(`https://analytics.arivihan.com${path}`, options)
-        .then(res => {
-            if (res.status === 200) {
-                return res.json()
-            } else if (res.status === 401) {
-                console.log(res);
-                return res.json();
-            }
-        })
-        .then(json => {
-            return json;
-        })
+  return fetch(`https://analytics.arivihan.com${path}`, options)
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json();
+      } else if (res.status === 401) {
+        console.log(res);
+        return res.json();
+      }
+    })
+    .then((json) => {
+      return json;
+    });
 }
 
-
-export {
-    smeCustomRequest, analyticsCustomRequest
-}
-
+export { smeCustomRequest, analyticsCustomRequest };
